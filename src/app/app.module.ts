0a8f91bd7e04937ab365e6c537626007f950a372
mm <>
2022-03-31 08:03:49 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/general/home/home.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { AboutComponent } from './modules/general/about/about.component';
+import { LoginComponent } from './modules/general/login/login.component';
+import { SignupComponent } from './modules/general/signup/signup.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    LoginComponent,
+    SignupComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
